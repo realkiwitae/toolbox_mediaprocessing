c731c5a83e51426a6170ab216afe923e336d6e21
realkiwitae <taelefebvre@gmail.com>
2023-07-11 17:03:30 +0200

    Watermark - add desired logo to inputs in folder imgs and videos

---
+++ watermark/README.md
@@ -0,0 +1,3 @@
+# Watermark
+Take a filder in input, to check for media, and a logo to add as watermark.
+Usage build/watermark -i <input_folder> -l <logo> -o <output_folder> -p <position> -s <size>
---
+++ watermark/main.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <sys/stat.h>
+#include <dirent.h>
+
+void treatImage(std::string img_path,cv::Mat logo_img,std::string output_folder, int id);
+void treatVideo(std::string file_path,cv::Mat logo_img,std::string output_folder, int id);
+void addLogo(cv::Mat& img, cv::Mat logo_img);
+
+int main(int argc, char* argv[]) {
+
+ // Usage build/watermark -i <input_folder> -l <logo> -o <output_folder> -p <position> -s <size>
+
+    if(argc != 7) {
+        std::cout << "Usage: " << argv[0] << " -i <input_folder> -l <logo> -o <output_folder>" << std::endl;
+        return 1;
+    }
+
+    std::string input_folder, logo, output_folder;
+    
+    for (int i = 1; i < argc; i++) {
+        if (std::string(argv[i]) == "-i") {
+            input_folder = argv[i + 1];
+            i++;
+        } else if (std::string(argv[i]) == "-l") {
+            logo = argv[i + 1];
+            i++;
+        } else if (std::string(argv[i]) == "-o") {
+            output_folder = argv[i + 1];
+            i++;
+        }
+    }
+
+    // read log
+    cv::Mat logo_img = cv::imread(logo, cv::IMREAD_UNCHANGED);
+    if (logo_img.empty()) {
+        std::cout << "Could not read the image: " << logo << std::endl;
+        return 1;
+    }
+
+    // create output folder if not exist
+    mkdir(output_folder.c_str(), 0777);
+
+    // for each image in input_folder
+    DIR *dir;
+    struct dirent *ent;
+
+    dir = opendir(input_folder.c_str());
+
+    // rename images as img_0001.png, img_0002.png, ...
+    // rename videos as vid_0001.mp4, vid_0002.mp4, ...
+    int img_count = 0;
+    int vid_count = 0;
+
+    while ((ent = readdir(dir)) != NULL) {
+        // check if file is image using cv::imread
+        std::string file_name = ent->d_name;
+        std::string file_path = input_folder + "/" + file_name;
+        cv::Mat img = cv::imread(file_path, cv::IMREAD_UNCHANGED);
+        if (img.empty()) {
+            treatVideo(file_path, logo_img, output_folder, ++vid_count);
+        }else{
+            treatImage(file_path, logo_img, output_folder, ++img_count);
+        }
+ 
+
+
+
+
+        
+    }
+
+    return 0;
+}
+
+void treatImage(std::string img,cv::Mat logo_img,std::string output_folder, int id){
+    
+    //print processing...
+    std::cout << "Processing image: " << img << std::endl;
+
+    // read image
+    cv::Mat img_mat = cv::imread(img, cv::IMREAD_UNCHANGED);
+    addLogo(img_mat, logo_img);
+    
+    std::string img_count_str = std::to_string(id);
+    img_count_str = std::string(2 - img_count_str.length(), '0') + img_count_str;
+    std::string output_path = output_folder + "/img_" + img_count_str + ".jpg"; 
+    cv::imwrite(output_path, img_mat);
+
+}
+void treatVideo(std::string file_path,cv::Mat logo_img,std::string output_folder, int id){
+
+    // check file extension
+    std::string file_name = file_path.substr(file_path.find_last_of("/\\") + 1);
+    std::string file_ext = file_name.substr(file_name.find_last_of(".") + 1);
+    if(file_ext != "mp4"){
+        return;
+    }
+
+    // check if file is a video using cv::VideoCapture
+    cv::VideoCapture cap(file_path);
+    if (!cap.isOpened()) {
+        std::cout << "Could not open the video file: " << file_path << std::endl;
+        return;
+    }
+
+    // print processing...
+    std::cout << "Processing video: " << file_path << std::endl;
+
+    // get video properties
+    int frame_width = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    int frame_height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    double fps = cap.get(cv::CAP_PROP_FPS);
+   // int frame_count = cap.get(cv::CAP_PROP_FRAME_COUNT);
+
+    // create output video
+    std::string vid_count_str = std::to_string(id);
+    vid_count_str = std::string(2 - vid_count_str.length(), '0') + vid_count_str;
+    std::string output_path = output_folder + "/vid_" + vid_count_str + ".mp4";
+    
+    std::string tmp_video = "/tmp/tmp.mp4";
+    cv::VideoWriter video(tmp_video, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), fps, cv::Size(frame_width, frame_height));
+
+    // for each frame add the logo
+    cv::Mat frame;
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) {
+            break;
+        }
+        addLogo(frame, logo_img);
+
+        // // show frame
+        // cv::imshow("Frame", frame);
+        // cv::waitKey(30);
+
+        //convert frame to rgb 
+        cv::cvtColor(frame, frame, cv::COLOR_BGRA2BGR);
+
+        video.write(frame);
+    }
+    // // keep sound from original video
+    std::string cmd = "ffmpeg -i " + file_path + " -i " + tmp_video + " -c copy -map 1:v:0 -map 0:a:0 " + output_path;
+    // silence output
+    cmd += " -loglevel panic";
+
+    system(cmd.c_str());
+     // remove tmp video
+    cmd = "rm " + tmp_video;
+    system(cmd.c_str());
+
+    // free vieo capture
+    cap.release();
+    video.release();
+
+}
+
+
+void addLogo(cv::Mat& img, cv::Mat logo_img){
+    // if no alpha channel, add alpha channel
+    if (img.channels() == 3) {
+        cv::cvtColor(img, img, cv::COLOR_BGR2BGRA);
+    }
+    // resize logo to fit in the low right corner, at 10% of the image height at least 100px, size of logo should be 10% of the image height
+    cv::Mat logo_resized;
+    // logo size = 10% of image height or 100px
+    int logo_size = img.rows / 10;
+    cv::resize(logo_img, logo_resized, cv::Size(int(logo_size*(double)logo_img.cols/logo_img.rows) , logo_size));
+
+    // add logo in bottom right corner + 10% of the image height
+    cv::Rect roi = cv::Rect(logo_size/4, logo_size/4, logo_resized.cols, logo_resized.rows);
+    cv::Mat destinationROI = img(roi);
+    cv::addWeighted(destinationROI, 1.0, logo_resized, 1., 0.0, destinationROI);
+
+}
